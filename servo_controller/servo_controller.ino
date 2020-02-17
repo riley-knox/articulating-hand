11b39b7d46a77e3a7a00da2946d0ee88190adc89
Riley Knox <rileyknox2020@u.northwestern.edu>
2020-02-17 17:24:39 -0600

    Custom ServoDrive message
        - Fixed CMakeLists.txt to allow custom message generation
    
        - Created ServoDrive custom message
            Two fields: servo # and angle to set
    
        - Integrated custom message with servo_controller.ino

--- servo_controller/servo_controller.ino
+++ servo_controller/servo_controller.ino
@@ -1,22 +1,30 @@
 #include <ros.h>
 #include <Servo.h>
 #include <Adafruit_PWMServoDriver.h>
+#include <articulating_hand/ServoDrive.h>
 
 #define SERVO_FREQ 50         
 
 ros::NodeHandle nh;
 
-// actuate callback function
+// actuation callback function
+void servo_callback(const articulating_hand::ServoDrive& cmd){
+  ;
+}
+
+// initialize subscriber
+ros::Subscriber<articulating_hand::ServoDrive> servo_sub("servo_cmd", &servo_callback);
 
 void setup() {
   // setup functions
   // largely cribbed from a variety of examples, MAY NEED TO ALTER THINGS A BIT
-  nh.initNode();        // initialize node
+  nh.initNode();            // initialize node
+
+  nh.subscribe(servo_sub);  // initialize subscriber
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   nh.spinOnce();
   delay(1);
 }
