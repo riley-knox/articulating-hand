ee6f85ca554a79bc3885ca44d5aa57a0e2682eea
Riley Knox <rileyknox2020@u.northwestern.edu>
2020-02-18 00:00:26 -0600

    Servo control via publisher
        - launch/hand_bot.launch
            Launches Arduino as node with correct port & baud rate
    
        - servo_controller.ino
            Initializes servo driver board criteria
            Subscriber callback:
                Pulls message inputs and checks for validity
                Lights appropriate LED (onboard/external)
                Sets servo position

--- servo_controller/servo_controller.ino
+++ servo_controller/servo_controller.ino
@@ -3,13 +3,43 @@
 #include <Adafruit_PWMServoDriver.h>
 #include <articulating_hand/ServoDrive.h>
 
-#define SERVO_FREQ 50         
+#define SERVO_FREQ 50
+#define SERVO_MIN 10
+#define SERVO_MAX 5000
+#define NUM_SERVOS 11
 
+// instantiate node handle; allows publisher/subscriber creation and handles serial communication
 ros::NodeHandle nh;
+
+// instantiate servo driver board interfacing
+Adafruit_PWMServoDriver srvDrvr = Adafruit_PWMServoDriver();
 
 // actuation callback function
 void servo_callback(const articulating_hand::ServoDrive& cmd){
-  ;
+  uint8_t servoNum = cmd.servo_num;           // servo number
+  uint16_t servoPos = cmd.servo_pos;          // servo position
+
+  
+  if ((0 <= servoNum) && (servoNum < NUM_SERVOS) && 
+      (SERVO_MIN < servoPos) && (servoPos < SERVO_MAX)){
+    digitalWrite(13, HIGH);           // turn on onboard LED
+
+    uint16_t pulseLength = servoPos;
+
+//    for (uint16_t pulseLength = servoPos; pulseLength < SERVO_MAX; pulseLength++){
+    srvDrvr.setPWM(servoNum, 0, pulseLength);
+//    }
+
+    delay(500);
+
+    digitalWrite(13, LOW);            // turn off onboard LED
+  }
+  else {                      // if servo number or position is out of range
+    digitalWrite(10, HIGH);   // turn on external LED
+    delay(1000);              // wait 1s
+    digitalWrite(10, LOW);    // turn off external LED
+  }
+  
 }
 
 // initialize subscriber
@@ -17,11 +47,16 @@ ros::Subscriber<articulating_hand::ServoDrive> servo_sub("servo_cmd", &servo_cal
 
 void setup() {
   // setup functions
-  // largely cribbed from a variety of examples, MAY NEED TO ALTER THINGS A BIT
   nh.initNode();            // initialize node
 
   nh.subscribe(servo_sub);  // initialize subscriber
 
+  srvDrvr.begin();
+  srvDrvr.setOscillatorFrequency(27000000);
+  srvDrvr.setPWMFreq(SERVO_FREQ);
+
+  pinMode(10, OUTPUT);      // external LED pin
+  pinMode(13, OUTPUT);      // onboard LED pin
 }
 
 void loop() {
